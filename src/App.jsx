d88307801959698df968289b2462e8cbc201c4d5
Erick-Melo <erick.coelho2013@hotmail.com>
2023-03-01 12:19:51 -0300

    instalacao de bibliotecas

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Home from './components/Home/Home'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  library.add(fas)
+  library.add(far)
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Home />
     </div>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function Home() {
+  return (
+    <>
+      Home Page
+      <div className="font-bold">
+        Teste Tailwind
+        <FontAwesomeIcon icon="fa-solid fa-arrow-up" />
+      </div>
+    </>
+  );
+}
